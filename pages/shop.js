9ce791bc48f14e82866e9fd8158caa7d204e13a0
root-kawaii <teo.regge.99@gmail.com>
2022-05-03 19:34:32 +0200

    v0.53

--- pages/shop.js
+++ pages/shop.js
@@ -1,8 +1,6 @@
 import Layout from "../components/layouts/article"
 import { Heading } from "@chakra-ui/react"
-import { SimpleGrid } from "@chakra-ui/react"
-import { WorkGridItem } from "../components/grid-item"
-import Section from "../components/section"
+
 
 
 
@@ -11,18 +9,6 @@ const shop = () => (
         <Heading as="h3" fontSize={20} mb={4}>
           Merch  
         </Heading>
-        <SimpleGrid columns={[1]} gap={12}>
-        <Section>
-          <WorkGridItem id="java_game" title="T-shirt" thumbnail={tshirt}>
-            Comfort t-shirt<br></br>For everyday use<br></br>
-          </WorkGridItem>
-        </Section>
-        <Section>
-          <WorkGridItem id="java_game" title="T-shirt" thumbnail={tshirt}>
-            Comfort t-shirt<br></br>For everyday use<br></br>
-          </WorkGridItem>
-        </Section>
-        </SimpleGrid>
         </Layout>
 )
 
